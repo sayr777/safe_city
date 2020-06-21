1081642e5b94b8bea8cb75a49f640998389b655b
Anton Tenyakov <a.tenyakov@t1-group.ru>
2020-06-21 13:07:53 +0300

    add added token verification in url

--- app.py
+++ app.py
@@ -13,6 +13,9 @@ dotenv.load_dotenv(dotenv_path)
 
 # DEBUG_METRICS = os.environ['DEBUG_METRICS']
 PROMETHEUS_PORT = os.environ['PROMETHEUS_PORT']
+TOKEN = os.environ['TOKEN']
+
+
 
 from prometheus_flask_exporter import PrometheusMetrics
 metrics = PrometheusMetrics(app=None, path='/metrics')
@@ -24,35 +27,55 @@ print(" * gRPC_URL: %s" % gRPC_URL)
 pSQL_URL = os.environ['pSQL_URL']
 print(" * pSQL_URL: %s" % pSQL_URL)
 
+API_TOKEN = os.environ['API_TOKEN']
+print(" * API_TOKEN: %s" % API_TOKEN)
 
-
-@app.route('/info')
-def hello():
-    return 'API интеграциии РНИС Нижегородской области с АПК "Безопасный город"'
+#Метод получения информации о сервере
+@app.route('/info/<path:token>')
+def hello(token):
+    if token == API_TOKEN:
+        return '[{"error":"0"},\n {"info": "API интеграциии РНИС Нижегородской области с АПК Безопасный город"}]'
+    else:
+        return '[{"error":"7"},\n {"info": "Предоставлен некорректный ключ"}]'
 
 #Метод обновления справочников ТС, огранизаций, типов ТС, БНСО
-@app.route('/v.1/update_dics', methods=['GET'])
-def update_dics():
-    return 'Ок'
+@app.route('/v.1/update_dics/<path:token>', methods=['GET'])
+def update_dics(token):
+    if token == API_TOKEN:
+        return 'Ок'
+    else :
+        return '[{"error":"7"},\n {"info": "Предоставлен некорректный ключ"}]'
 
 #Метод получения словаря ТС
-@app.route('/v.1/vehicles_dic', methods=['GET'])
-def get_vehicles():
-    return json.dumps(get_vehicles_dic())
+@app.route('/v.1/vehicles_dic/<path:token>', methods=['GET'])
+def get_vehicles(token):
+    if token == API_TOKEN:
+        response = json.dumps(get_vehicles_dic())
+        return
+    else :
+        return '[{"error":"7"},\n {"info": "Предоставлен некорректный ключ"}]'
 
 #Метод получения последнего телематического сообщения по DeviceCode
-@app.route('/v.1/messages/idDev:<path:idDev>', methods=['GET'])
-def get_telematics_by_device(idDev):
+@app.route('/v.1/messages/idDev:<path:idDev>/<path:token>', methods=['GET'])
+def get_telematics_by_device(idDev,token):
     if idDev == '':
         abort(404)
-    return get_t_by_device(idDev,gRPC_URL)
+    if token == API_TOKEN:
+        return get_t_by_device(idDev,gRPC_URL)
+    else :
+        return '[{"error":"7"},\n {"info": "Предоставлен некорректный ключ"}]'
 
 #Метод получения телематических сообщений по uuid предприятия
-@app.route('/v.1/messages/idOrg:<path:idOrg>', methods=['GET'])
-def get_telematics_by_org(idOrg):
+@app.route('/v.1/messages/idOrg:<path:idOrg>/<path:token>', methods=['GET'])
+def get_telematics_by_org(idOrg,token):
     if idOrg == '':
         abort(404)
-    return json.dumps(get_t_by_org(idOrg,gRPC_URL))
+    if token == API_TOKEN:
+        return json.dumps(get_t_by_org(idOrg,gRPC_URL))
+    else :
+        return '[{"error":"7"},\n {"info": "Предоставлен некорректный ключ"}]'
+
+
 
 if __name__ == '__main__':
     metrics.init_app(app)
